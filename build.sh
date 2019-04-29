ae413f4f7585a59f0dd1cc216ed4cb76f5cb7901
Abdelhak Marourane <amarouane@itsc.uah.edu>
2019-04-29 12:27:04 -0500

    Adding envs

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@
 cd $HOME/ghrc-deploy
 npm install
 npm run  build
+./node_modules/.bin/kes cf compile --kes-folder app --region $AWS_REGION --deployment $deployment_name --template node_modules/@cumulus/deployment/app --role $roles
